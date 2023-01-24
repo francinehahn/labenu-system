6465e5bd57d7e73d1513cd032d29dd3c01c9957d
Francine Hahn <fran_hahn@hotmail.com>
2023-01-23 23:52:38 -0300

    deploy

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,14 @@
 import express from 'express'
 import cors from 'cors'
-import { AddressInfo } from 'net'
+
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-const server = app.listen(process.env.PORT || 3003, () => {
-    if (server) {
-        const address = server.address() as AddressInfo
-        console.log(`Server is running in http://localhost:${address.port}`)
-    } else {
-        console.error(`Failure upon starting server.`)
-    }
+app.listen(process.env.PORT || 3003, () => {
+    console.log(`Server is running in http://localhost:${process.env.PORT}`)
 })
 
 export default app
